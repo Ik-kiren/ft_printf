99e777d14a7bb66ef7099030eb7ec4701771355d
Chris Dupuis <cdupuis@k2r3p9.42mulhouse.fr>
2023-03-23 13:23:17 +0100

    correction

--- main_test.c
+++ main_test.c
@@ -6,7 +6,7 @@
 /*   By: cdupuis <cdupuis@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/15 11:20:53 by cdupuis           #+#    #+#             */
-/*   Updated: 2022/12/15 11:48:31 by cdupuis          ###   ########.fr       */
+/*   Updated: 2023/03/23 13:20:22 by cdupuis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,21 @@
 
 int	main(int argc, char **argv)
 {
-	printf("t1(%i)", ft_printf(" %x ", 450));
-	printf("%c", '\n');
-	printf("t2(%i)", printf(" %x ", 450));
-	printf("%c", '\n');
-	printf("t1(%i)", ft_printf(" %c", '0' - 256));
-	printf("%c", '\n');
-	printf("t2(%i)", printf(" %c", '0' - 256));
-	printf("%c", '\n');
-	printf("t1(%i)", ft_printf("%c%c%c", 'a', '\t', 'b'));
-	printf("%c", '\n');
-	printf("t2(%i)", printf("%c%c%c", 'a', '\t', 'b'));
-	printf("%c", '\n');
+	char	*p;
+
+	p = 'c';
+	ft_printf(" %x %X \n", 450, 9999);
+	printf(" %x %X\n", 450, 9999);
+	ft_printf(" %X \n", 4780);
+	printf(" %X \n", 4780);
+	ft_printf(" %c \n", '0' - 256);
+	printf(" %c \n", '0' - 256);
+	ft_printf("%c%c%c \n", 'a', '\t', 'b');
+	printf("%c%c%c  \n", 'a', '\t', 'b');
+	ft_printf("%d\n", INT_MAX);
+	printf("%d\n", INT_MAX);
+	ft_printf("%d\n", INT_MIN);
+	printf("%d\n", INT_MIN);
+	ft_printf("%p\n", p);
+	printf("%p\n", p);
 }
